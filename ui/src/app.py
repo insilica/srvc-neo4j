77ab4147fd08c59896045560fb31c14eabe8e5c7
John Shaffer <john@insilica.co>
2023-07-06 14:16:12 -0500

    Add UI component and show other components in tabbed iframes

---
+++ ui/src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    document_port = os.getenv('DOCUMENT_PORT')
+    review_port = os.getenv('REVIEW_PORT')
+    upload_port = os.getenv('UPLOAD_PORT')
+    return render_template('index.html', document_port=document_port, review_port=review_port, upload_port=upload_port)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')  # Ensure the server is accessible externally
