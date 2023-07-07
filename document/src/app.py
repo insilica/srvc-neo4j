75b29e594590e321bf0f2e5fd202fc862f0216c8
John Shaffer <john@insilica.co>
2023-07-07 18:53:34 -0500

    Initial work on jena/sparql

--- document/src/app.py
+++ document/src/app.py
@@ -1,26 +1,51 @@
 from flask import Flask, request, render_template
-from jsonlines.jsonlines import InvalidLineError
-from werkzeug.utils import secure_filename
-from py2neo import Graph, Node, Relationship
-from py2neo.bulk import merge_nodes
+from SPARQLWrapper import SPARQLWrapper, JSON
 from uuid import uuid4
-import json, jsonlines, os, uuid
+import json, os
 
 app = Flask(__name__)
 
+sparql_endpoint = 'http://jena:3030/srvc/query'  # Update the SPARQL endpoint URL to match your Jena Fuseki configuration
+username = 'admin'
+password = 'test1234'
+
 @app.route('/document')
 def list_documents():
-    graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
-    documents = graph.run("""
-    MATCH (d:Object {type: "document"})
-    OPTIONAL MATCH (d)<-[:HAS_EVENT]-(a:Object {type: "label-answer"})-[:HAS_LABEL]->(l:Object)
-    RETURN d.uri AS uri, d.json_data AS json_data,
-           collect({label: l, answer: a}) AS label_answers
-    """).data()
+    sparql = SPARQLWrapper(sparql_endpoint)
+    sparql.setCredentials(username, password)  # Set the credentials for authentication
+    sparql.setQuery('''
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX neo: <http://neo4j.org#>
+        SELECT ?uri ?json_data ?label ?answer WHERE {
+            ?d rdf:type neo:Object ;
+               neo:type "document" ;
+               neo:uri ?uri ;
+               neo:json_data ?json_data .
+            OPTIONAL {
+                ?d neo:HAS_EVENT ?a .
+                ?a rdf:type neo:Object ;
+                   neo:type "label-answer" ;
+                   neo:answer ?answer .
+                ?a neo:HAS_LABEL ?l .
+                ?l neo:question ?label .
+            }
+        }
+    ''')
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
-    for document in documents:
-        document['json_data'] = json.loads(document['json_data'])
-        document['label_answers'] = [(x['label'], x['answer']) for x in document['label_answers'] if x['label'] and x['answer']]
+    documents = []
+    for result in results["results"]["bindings"]:
+        document = {
+            "uri": result["uri"]["value"],
+            "json_data": json.loads(result["json_data"]["value"]),
+            "label_answers": []
+        }
+        if "label" in result and "answer" in result:
+            label = result["label"]["value"]
+            answer = result["answer"]["value"]
+            document["label_answers"].append((label, answer))
+        documents.append(document)
 
     return render_template('documents.html', documents=documents)
 
--- upload/src/app.py
+++ upload/src/app.py
@@ -1,62 +1,43 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
 from flask import send_from_directory
-
-from jsonlines.jsonlines import InvalidLineError
-from werkzeug.utils import secure_filename
-from py2neo import Graph, Node, Relationship
-from py2neo.bulk import merge_nodes
+from rdflib import Graph, Literal, Namespace, URIRef
+from rdflib.plugins.sparql import prepareQuery
+from SPARQLWrapper import SPARQLWrapper, SPARQLWrapper2, DIGEST, JSON
 from uuid import uuid4
-import json, jsonlines, os, uuid
+from werkzeug.utils import secure_filename
+import json, os, requests
 
 app = Flask(__name__)
-
 app.config['UPLOAD_FOLDER'] = '/tmp/srvc-upload'
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-def create_nodes(data, filename):
-    document = Node('document', id=str(uuid4()), json_data=json.dumps(data), type=data.get('type'), uri=data.get('uri'))
-    document_source = Node('document_source', name=filename)
-    rel = Relationship(document_source, 'SOURCE_OF', document)
-    return document, document_source, rel
+sparql_endpoint = 'http://jena:3030/srvc/'
+username = 'admin'
+password = 'test1234'
 
-def upload_to_neo4j(file_path, filename, graph):
-    with open(file_path, 'r') as f:
-        documents = []
-        document_sources = []
-        rels = []
-        for line in f:
-            line = line.strip()  # remove leading/trailing whitespace
-            if line:  # skip blank lines
-                try:
-                    data = json.loads(line) # try to parse line as JSON
-                    document, document_source, rel = create_nodes(data, filename)
-                    documents.append(document)
-                    document_sources.append(document_source)
-                    rels.append(rel)
-
-                except json.JSONDecodeError:
-                    print(f"Skipped invalid JSON line: {line}")
-                    continue
-
-        tx = graph.begin()
-        for document, document_source, rel in zip(documents, document_sources, rels):
-            tx.merge(document_source, "document_source", "name")
-            tx.merge(document, "document", "id")
-            tx.create(rel)
-        graph.commit(tx)
-
-def delete_from_neo4j(source_name, graph):
-    # delete relationships first
-    # TODO this looks wrong to me. We should delete the source, and the dangling documents.
-    graph.run("MATCH (s:document_source)-[r:SOURCE_OF]->(d:document) WHERE s.name = $name DELETE r", name=source_name)
-    graph.run("MATCH (s:document_source)-[:SOURCE_OF]->(d:document) WHERE s.name = $name DELETE s, d", name=source_name)
-    
 @app.route('/upload')
 def upload():
-    graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
-    result = graph.run("MATCH (s:document_source)-[:SOURCE_OF]->(d:document) RETURN s.name as source, count(d) as documents").data()
-    return render_template('upload.html', sources=result)
+    sparql = SPARQLWrapper(sparql_endpoint)
+    sparql.setCredentials(username, password)  # Set the credentials for authentication
+    sparql.setQuery('''
+        PREFIX neo: <http://neo4j.org#>
+        SELECT ?source (COUNT(?d) AS ?documents) WHERE {
+            ?s neo:name ?source .
+            ?s neo:SOURCE_OF ?d .
+        }
+        GROUP BY ?source
+    ''')
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    sources = []
+    for result in results["results"]["bindings"]:
+        source = result["source"]["value"]
+        documents = int(result["documents"]["value"])
+        sources.append({"source": source, "documents": documents})
+
+    return render_template('upload.html', sources=sources)
 
 @app.route('/upload/source', methods=['POST'])
 def upload_file():
@@ -64,21 +45,77 @@ def upload_file():
         f = request.files['file']
         filename = secure_filename(f.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        f.save(file_path) # TODO we probably need a better way to persist files
+        f.save(file_path)
 
-        graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
-        upload_to_neo4j(file_path, filename, graph)
-        
+        with open(file_path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    try:
+                        data = json.loads(line)
+                        triples = create_triples(data, filename)
+
+                        if not triples:  # Skip graph update if no triples are generated
+                            continue
+
+                        for triple in triples:
+                            subject, predicate, obj = triple
+
+                            # SPARQL query to insert the triple into the graph
+                            insert_query = '''
+                            PREFIX neo: <http://neo4j.org#>
+                            INSERT DATA {{
+                                GRAPH <http://example.com/data> {{
+                                    {} {} {} .
+                                }}
+                            }}
+                            '''.format(subject.n3(), predicate.n3(), obj.n3())
+
+                            # Send the SPARQL query to the Fuseki endpoint
+                            response = requests.post('http://jena:3030/srvc/update', data=insert_query, auth=(username, password))
+                            if response.status_code != 204:
+                                print(f"Failed to insert triple: {insert_query}")
+
+                    except json.JSONDecodeError:
+                        print(f"Skipped invalid JSON line: {line}")
+                        continue
+
         return redirect(url_for('upload'))
 
 @app.route('/upload/delete/<source_name>', methods=['POST'])
 def delete_source(source_name):
     if request.method == 'POST':
-        graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
-        delete_from_neo4j(source_name, graph)
-        
+        sparql = SPARQLWrapper(sparql_endpoint)
+        sparql.setCredentials(username, password)  # Set the credentials for authentication
+        delete_from_jena(source_name, sparql)
+
         return redirect(url_for('upload'))
 
+def create_triples(data, filename):
+    triples = [
+        (URIRef('http://neo4j.org#document'), URIRef('http://neo4j.org#id'), Literal(str(uuid4()))),
+        (URIRef('http://neo4j.org#document'), URIRef('http://neo4j.org#json_data'), Literal(json.dumps(data))),
+        (URIRef('http://neo4j.org#document'), URIRef('http://neo4j.org#type'), Literal(data.get('type'))),
+        (URIRef('http://neo4j.org#document'), URIRef('http://neo4j.org#uri'), Literal(data.get('uri'))),
+        (URIRef('http://neo4j.org#document_source'), URIRef('http://neo4j.org#name'), Literal(filename)),
+        (URIRef('http://neo4j.org#document_source'), URIRef('http://neo4j.org#SOURCE_OF'), URIRef('http://neo4j.org#document')),
+    ]
+    return triples
+
+def delete_from_jena(source_name, sparql):
+    delete_query = '''
+    PREFIX neo: <http://neo4j.org#>
+    DELETE WHERE {
+        GRAPH <http://example.com/data> {
+            ?s neo:name "%s" ;
+               neo:SOURCE_OF ?d .
+        }
+    }
+    ''' % source_name
+
+    sparql.setQuery(delete_query)
+    sparql.setMethod('POST')
+    sparql.query()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')  # Ensure the server is accessible externally
