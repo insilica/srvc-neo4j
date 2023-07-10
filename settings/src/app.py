4c03c9cba5a510a9ef32ad18dfc1b4dc0bd4bc39
Thomas Luechtefeld <tom@insilica.co>
2023-07-09 21:31:19 -0400

    minor ui update

--- settings/src/app.py
+++ settings/src/app.py
@@ -1,8 +1,36 @@
-from flask import Flask, request, render_template, redirect, url_for
-from py2neo import Graph, Node
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from py2neo import Graph, Node, NodeMatcher
 from uuid import uuid4
+import json
 
 app = Flask(__name__)
+
+settings_group = [
+    {'name': 'Project', 'settings': ['visibility']},
+    {'name': 'Review', 'settings': ['review_priority', 'unlimited_reviewers']},
+    {'name': 'Access', 'settings' : ['invite_link']}
+]
+
+default_settings = [
+    {'key': 'visibility', 'value': 'public', 'options': ['public', 'private'], 'enabled': False},
+    {'key': 'review_priority', 'value': 'public', 'options': ['public', 'private'], 'enabled': False},
+    {'key': 'unlimited_reviewers', 'value': 'No', 'options': ['Yes', 'No']},
+    {'key': 'invite_link', 'value': None, 'options': None, 'enabled': False}
+]
+
+@app.before_first_request
+def create_settings():
+    graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
+    matcher = NodeMatcher(graph)
+    for group in settings_group:
+        for setting_key in group['settings']:
+            setting = next((item for item in default_settings if item['key'] == setting_key), None)
+            if setting:
+                if matcher.match("Setting", key=setting_key).first() is None:
+                    tx = graph.begin()
+                    setting_node = Node("Setting", key=setting_key, value=setting['value'], options=json.dumps(setting['options']), enabled=setting['enabled'], group=group['name'])
+                    tx.create(setting_node)
+                    tx.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def settings():
@@ -16,9 +44,13 @@ def settings():
         tx.commit()
         return redirect(url_for('settings'))
 
-    settings = graph.run("MATCH (setting:Setting) RETURN setting.key AS key, setting.value AS value").data()
-    return render_template('settings.html', settings=settings)
+    settings = graph.run("MATCH (setting:Setting) RETURN setting.key AS key, setting.value AS value, setting.options AS options, setting.enabled AS enabled, setting.group AS group").data()
+    return render_template('settings.html', settings=settings, settings_group=settings_group)
 
+@app.route('/generate_invite_link', methods=['GET'])
+def generate_invite_link():
+    invite_id = str(uuid4())
+    return jsonify({'invite_link': invite_id})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
