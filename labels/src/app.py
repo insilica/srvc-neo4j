449458ee675ee29bf1fef6ca0900ee1df3a0a1ea
Thomas Luechtefeld <tom@insilica.co>
2023-07-16 11:38:30 -0400

    update ui for labels

--- labels/src/app.py
+++ labels/src/app.py
@@ -1,29 +1,63 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from py2neo import Graph, Node
 from uuid import uuid4
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/create', methods=['POST'])
+def create_label():
+    graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
+    id = str(uuid4())
+    name = request.form.get('name')
+    description = request.form.get('description')
+    label_type = request.form.get('type')
+
+    if name and description and label_type:  # Ensure no empty fields
+        tx = graph.begin()
+        tx.run("""
+            MERGE (label:Label {id: $id, name: $name, description: $description, type: $type})
+            """, id=id, name=name, description=description, type=label_type)
+        tx.commit()
+
+    return jsonify({'status': 'success', 'message': 'Label created successfully'})
+
+@app.route('/edit/<id>', methods=['POST'])
+def edit_label(id):
+    graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
+    name = request.form.get('name')
+    description = request.form.get('description')
+    label_type = request.form.get('type')
+
+    if name and description and label_type:  # Ensure no empty fields
+        tx = graph.begin()
+        tx.run("""
+            MATCH (label:Label {id: $id}) 
+            SET label.name = $name, label.description = $description, label.type = $type
+            """, id=id, name=name, description=description, type=label_type)
+        tx.commit()
+
+    return jsonify({'status': 'success', 'message': 'Label edited successfully'})
+
+@app.route('/delete/<id>', methods=['POST'])
+def delete_label(id):
+    graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
+
+    # TODO also delete any answers that have this label
+    tx = graph.begin()
+    tx.run("""
+        MATCH (label:Label {id: $id}) DETACH DELETE label
+        """, id=id)
+    tx.commit()
+
+    return jsonify({'status': 'success', 'message': 'Label deleted successfully'})
+
+@app.route('/', methods=['GET'])
 def label_editor():
     graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
 
-    if request.method == 'POST':
-        id = str(uuid4())
-        name = request.form.get('name')
-        description = request.form.get('description')
-        label_type = request.form.get('type')
-
-        if name and description and label_type:  # Ensure no empty fields
-            tx = graph.begin()
-            tx.run("""
-                MERGE (label:Label {id: $id, name: $name, description: $description, type: $type})
-                """, id=id, name=name, description=description, type=label_type)
-            tx.commit()
-
-        return redirect(url_for('label_editor'))
-
-    labels = graph.run("MATCH (label:Label) RETURN label.id AS id, label.name AS name, label.description AS description, label.type AS type").data()
+    q = "MATCH (l:Label) RETURN l.id AS id, l.name AS name,"
+    q = f"{q} l.description AS description, l.type AS type"
+    labels = graph.run(q).data()
     labels = list(enumerate(labels, start=1))  # Add index for each label
     return render_template('labels.html', labels=labels)
 
