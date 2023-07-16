a22054b294866aaac9e43ec2408c4400176ea9d1
Thomas Luechtefeld <tom@insilica.co>
2023-07-16 16:27:58 -0400

    progress on review UI and submitting answers

--- review/src/app.py
+++ review/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, jsonify
 from jsonlines.jsonlines import InvalidLineError
 from werkzeug.utils import secure_filename
 from py2neo import Graph, Node, Relationship
@@ -19,9 +19,10 @@ def get_node_by_id(tx, node_id):
     result = tx.run("MATCH (n {id: $id}) RETURN n", id=node_id)
     records = result.data()
     return records[0]['n'] if records else None
-
-@app.route('/review', methods=['POST'])
+    
+@app.route('/submit_review', methods=['POST'])
 def review_post():
+    print("HOLLA AT YA BOI")
     graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
     doc_id = request.form['doc-id']
     tx = graph.begin()
@@ -46,7 +47,7 @@ def review_post():
 
     graph.commit(tx)
 
-    return redirect('/review')
+    return jsonify({'status': 'success', 'message': 'Document reviewed successfully'})
 
 def get_unreviewed_document():
     graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test1234"))
